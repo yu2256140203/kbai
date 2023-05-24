9fd941547a775056c3e14aadcd38d42e896710dc
Peilun Jiang <peilunjuice@gmail.com>
2023-05-24 15:47:38 -0400

    Final commit

--- SemanticNetsAgent/main.py
+++ SemanticNetsAgent/main.py
@@ -38,6 +38,7 @@ def plot_time():
     #This will plot the time it takes for your agent
     #to solve the problem for different initial states.
     cases = [(1, 1), (2, 2), (3, 3), (5, 3), (6, 3), (7, 3), (5, 5)]
+    # cases = [(1, 1), (2, 2), (3, 3), (5, 3), (6, 3), (7, 3), (8, 3),(9, 3), (10, 3), (11, 3), (12, 3), (13, 3), (14, 3), (15, 3)]
     times = []
     test_agent = SemanticNetsAgent()
     for case in cases:
@@ -47,12 +48,28 @@ def plot_time():
         times.append(end_time - start_time)
     import matplotlib.pyplot as plt
     plt.plot([sum(c) for c in cases], times)
-    plt.xlabel("Sum of initial number of sheep and wolves")
+    plt.xlabel("# or animals")
     plt.ylabel("Time taken to solve problem")
-    plt.savefig("time taken and number of animals.png")
+    plt.savefig("Figure 1: Runtime vs. Total Animals.png")
+    plt.clf()
 
-
+def plot_time_more_cases():
+    cases = [(1, 1), (2, 2), (3, 3), (5, 3), (6, 3), (7, 3), (8, 3),(9, 3), (10, 3), (11, 3), (12, 3), (13, 3), (14, 3), (15, 3)]
+    times = []
+    test_agent = SemanticNetsAgent()
+    for case in cases:
+        start_time = time.time()
+        test_agent.solve(case[0], case[1])
+        end_time = time.time()
+        times.append(end_time - start_time)
+    import matplotlib.pyplot as plt
+    plt.plot([sum(c) for c in cases], times)
+    plt.xlabel("# or animals")
+    plt.ylabel("Time taken to solve problem")
+    plt.savefig("Figure 2: Runtime vs. Total Animals.png")
+    plt.clf()
 
 if __name__ == "__main__":
     test()
     plot_time()
+    plot_time_more_cases()
