0205e8cdcee46b40a9bf1f8ab4bb78c581073eb8
Peilun Jiang <peilunjuice@gmail.com>
2023-06-09 19:37:57 -0700

    week 3

---
+++ BlockWorldAgent/BlockWorldAgent.py
@@ -0,0 +1,34 @@
+class BlockWorldAgent:
+    def __init__(self):
+        #If you want to do any initial processing, add it here.
+        pass
+
+    def solve(self, initial_arrangement, goal_arrangement):
+        #Add your code here! Your solve method should receive
+		#as input two arrangements of blocks. The arrangements
+		#will be given as lists of lists. The first item in each
+		#list will be the bottom block on a stack, proceeding
+		#upward. For example, this arrangement:
+		#
+		#[["A", "B", "C"], ["D", "E"]]
+		#
+		#...represents two stacks of blocks: one with B on top
+		#of A and C on top of B, and one with E on top of D.
+		#
+		#Your goal is to return a list of moves that will convert
+		#the initial arrangement into the goal arrangement.
+		#Moves should be represented as 2-tuples where the first
+		#item in the 2-tuple is what block to move, and the
+		#second item is where to put it: either on top of another
+		#block or on the table (represented by the string "Table").
+		#
+		#For example, these moves would represent moving block B
+		#from the first stack to the second stack in the example
+		#above:
+		#
+		#("C", "Table")
+		#("B", "E")
+		#("C", "A")
+        pass
+
+
