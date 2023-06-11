8f2f07218fb789810c530c71648a1f52521fb44d
Peilun Jiang <peilunjuice@gmail.com>
2023-06-11 16:05:04 -0700

    final commit

--- BlockWorldAgent/BlockWorldAgent.py
+++ BlockWorldAgent/BlockWorldAgent.py
@@ -8,8 +8,6 @@ class State:
         self.parent = None
         self.move_from_parent_record = None # eg. ("C", "Table"), the move that was made to get to this state from the parent state
         self._build_map_block()
-        self.item_axis = {}
-        self.create_item_axis()
 
     def _build_map_block(self):
         ### block map ###
@@ -26,13 +24,6 @@ class State:
                 else:
                     map_block[stack[i]] = stack[i-1]
         self.map_block = map_block
-
-    def create_item_axis(self):
-        item_axis = {}
-        for block_idx, stack in enumerate(self.list_of_stacks):
-            for idx, item in enumerate(stack):
-                item_axis[item] = (block_idx, idx)
-        self.item_axis = item_axis
 
     def __eq__(self, other):
         pass
@@ -54,17 +45,17 @@ class State:
 
         return deepcopy(possible_moves)
 
-    def distance_old(self, goal_state):
-        score_goal = len(goal_state.map_block)
-        score_self = 0
-        for key, value in goal_state.map_block.items():
-            if self.map_block[key] == value:
-                score_self += 1
-        return score_goal - score_self
+    # def distance_old(self, goal_state):
+    #     score_goal = len(goal_state.map_block)
+    #     score_self = 0
+    #     for key, value in goal_state.map_block.items():
+    #         if self.map_block[key] == value:
+    #             score_self += 1
+    #     return score_goal - score_self
 
     def distance(self, goal_state):
         total_distance = 0
-        self.create_item_axis()
+
         self._build_map_block()
         for stack in self.list_of_stacks:
             for idx, item in enumerate(stack):
@@ -75,13 +66,7 @@ class State:
                         total_distance += 2*(len(stack) - idx)
                     break
         return total_distance
-        #for stack in goal_state.list_of_stacks:
-        #    table_item_x = self.item_axis[stack[0]][0]
-        #    for idx, item  in enumerate(stack):
-        #        x, y = self.item_axis[item]
-        #        if x!=table_item_x or y != idx:
-        #            total_distance += 1
-        return total_distance
+
 
     def move(self, possible_move):
         from_stack = possible_move[0]
@@ -107,29 +92,6 @@ class State:
                 self.list_of_stacks.pop(idx)
 
 
-#    def move(self, origin_index, destination_index):
-#        list_of_stacks = self.list_of_stacks.copy()
-#        if destination_index == "Table":
-#            move_from_parent_record = (list_of_stacks[origin_index][-1], "Table")
-#            list_of_stacks.append([list_of_stacks[origin_index].pop(0)])
-#        else:
-#            move_from_parent_record = (list_of_stacks[origin_index][-1], list_of_stacks[destination_index][-1])
-#            list_of_stacks[destination_index].insert(-1, list_of_stacks[origin_index].pop(0))
-#        new_state = State(list_of_stacks)
-#        new_state.parent = self
-#        new_state.move_from_parent_record = move_from_parent_record
-#        return new_state
-
-#    def find_children(self):
-#        num_stacks = len(self.list_of_stacks)
-#        for i in range(num_stacks):
-#            new_state = self.move(i, "Table")
-#            self.children.append(new_state)
-#            for j in range(num_stacks):
-#                if i != j:
-#                    new_state = self.move(i, j)
-#                    self.children.append(new_state)
-#
 class BlockWorldAgent:
     def __init__(self):
         #If you want to do any initial processing, add it here.
@@ -167,9 +129,7 @@ class BlockWorldAgent:
         self.goal_state = State(goal_arrangement)
         self.initial_state = State(initial_arrangement)
         self.move_strategy()
-        return self.path 
-      #  final_state = self._bfs(initial_state, goal_state)
-      #  return self._get_path_to_root(final_state).reverse()
+        return self.path
 
     def move_strategy(self):
 
@@ -190,31 +150,8 @@ class BlockWorldAgent:
                     break
                 else:
                     self.initial_state.reverse_move(possible_move)
-#            print("current state", self.initial_state.list_of_stacks, 'goal state:', self.goal_state.list_of_stacks)
+        # print("current state", self.initial_state.list_of_stacks, 'goal state:', self.goal_state.list_of_stacks)
 
-
-  #  def _bfs(self, root_state, goal_state):
-  #      queue = []
-  #      visited = []
-  #      queue.append(root_state)
-  #      visited.append(root_state)
-  #      while queue:
-  #          current = queue.pop(0)
-  #          if current.distance(goal_state) == 0:
-  #              return current
-  #          current.find_children()
-  #          for child in current.children:
-  #              if child not in visited:
-  #                  queue.append(child)
-  #                  visited.append(child)
-  #      return None
-
-  #  def _get_path_to_root(state):
-  #      path = []
-  #      while state.parent:
-  #          path.append(state.move_from_parent_record)
-  #          state = state.parent
-  #      return path
 
 
 
