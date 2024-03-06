b7a79920466896e31408fcae9b34b83c749bf2d4
Peilun Jiang <peilunjuice@gmail.com>
2024-03-06 14:08:22 -0500

    add Sentence Reading project

---
+++ SentenceReadingAgent/SentenceReadingAgent.py
@@ -0,0 +1,28 @@
+class SentenceReadingAgent:
+    def __init__(self):
+        #If you want to do any initial processing, add it here.
+        pass
+
+    def solve(self, sentence, question):
+    
+        '''
+          You can use a library like spacy (https://spacy.io/usage/linguistic-features) to preprocess the
+            mostcommon.txt file. There are others that could be used but you must use them in preprocessing only.
+            You CANNOT import the library into Gradescope.
+          
+          You must include whatever preprocessing you've done into your SentenceReadingAgent.py.
+          
+          DO NOT use another file .txt or .csv. Hard code your DICTS | LISTS into this .py file
+          
+          While the supplied mostcommon.txt contains most of the common words you will need
+            you can (and SHOULD) expand the file as you find cases that the agent has problems
+            processing. 
+            
+          Also not all words will be processed using the correct lexing for every possible problem the 
+            agent might encounter and you are ENCOURAGED to expand these in your agents knowledge representation.
+        '''
+    
+        #Add your code here! Your solve method should receive
+		#two strings as input: sentence and question. It should
+		#return a string representing the answer to the question.
+        pass
